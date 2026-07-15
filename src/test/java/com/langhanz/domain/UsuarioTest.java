60b72dcea723fd63411de0ce682d401a972b71d5
João Paulo Langhanz <joaopaulo.lang@gmail.com>
2026-07-14 23:06:37 -0300

    AssertAll

--- src/test/java/com/langhanz/domain/UsuarioTest.java
+++ src/test/java/com/langhanz/domain/UsuarioTest.java
@@ -1,5 +1,6 @@
 package com.langhanz.domain;
 
+import com.langhanz.domain.exception.ValidationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,20 @@ public class UsuarioTest {
     public void deveCriarUsuarioValido(){
         Usuario usr = new Usuario(1L, "Usuario valido", "user@email.com", "123456");
 
-        assertEquals(1L, usr.getId());
-        assertEquals("Usuario valido", usr.getNome());
-        assertEquals("user@email.com", usr.getEmail());
-        assertEquals("123456", usr.getSenha());
+        Assertions.assertAll(
+            () -> assertEquals(1L, usr.getId()),
+            () -> assertEquals("Usuario valido", usr.getNome()),
+            () -> assertEquals("user@email.com", usr.getEmail()),
+            () -> assertEquals("123456", usr.getSenha())
+        );
+    }
 
-
-
+    @Test
+    @DisplayName("Deve rejeitar usuário com nome nulo")
+    public void deveRejeitarUsuarioComNomeNulo(){
+        ValidationException ex = Assertions.assertThrows(ValidationException.class, () ->
+                new Usuario(1L, null, "email@email.com", "123123")
+        );
+        assertEquals("Nome é obrigatório.", ex.getMessage());
     }
 }
