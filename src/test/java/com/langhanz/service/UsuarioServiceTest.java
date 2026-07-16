eaa693967a025d0df55e6c08281d4e9bc89010be
João Paulo Langhanz <joaopaulo.lang@gmail.com>
2026-07-16 18:09:58 -0300

    Mockito - Mock thenReturn

--- src/test/java/com/langhanz/service/UsuarioServiceTest.java
+++ src/test/java/com/langhanz/service/UsuarioServiceTest.java
@@ -37,10 +37,39 @@ public class UsuarioServiceTest {
         service = new UsuarioService(repository);
 
         Mockito.when(repository.getUserByEmail("email@email.com"))
-                .thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()));
+                .thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()))
+                .thenReturn(null);
 
-        Optional<Usuario> user = service.getUserByEmail("mail@mail.com");
-        Assertions.assertTrue(user.isEmpty());
+        Optional<Usuario> user = service.getUserByEmail("email@email.com");
+        System.out.println(user);
+        user = service.getUserByEmail("email@email.com");
+        System.out.println(user);
+        Assertions.assertTrue(user.isPresent());
+
+        Mockito.verify(repository, Mockito.atLeastOnce()).getUserByEmail("email@email.com");
+    }
+
+
+    @Test
+    public void deveSalvarUsuarioComSucessoMock(){
+        UsuarioRepository repository = Mockito.mock(UsuarioRepository.class);
+        service = new UsuarioService(repository);
+
+        Usuario userToSave = UsuarioBuilder.umUsuario().comId(null).agora();
+
+         Mockito.when(repository.getUserByEmail(userToSave.getEmail()))
+                 .thenReturn(Optional.empty());
+
+         Mockito.when(repository.salvar(userToSave))
+                 .thenReturn(UsuarioBuilder.umUsuario().agora());
+
+         Usuario savedUser = service.salvar(userToSave);
+         Assertions.assertNotNull(savedUser.getId());
+
+         Mockito.verify(repository).getUserByEmail(userToSave.getEmail());
+//         Mockito.verify(repository).salvar(userToSave); // Não é necessario pois o assert já garante isso
+
+
     }
 
 
